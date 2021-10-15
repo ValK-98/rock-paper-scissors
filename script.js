e0691cecaf2ef4dd132fc36f50ce8ed0b9716659
Valentinas <vkornijenka@gmail.com>
2021-10-15 20:45:26 +1000

    created randomiser function

---
+++ script.js
@@ -0,0 +1,8 @@
+function computerPlay () {
+    const generator = 0;
+    for (let i = 0; i < 5; i++) {
+        let state = (Math.random()*10);
+        return state+generator;
+    }
+    return generator;
+}
