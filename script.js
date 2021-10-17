4c4941418ad8546fdfbbb17dda865efb94d0c9a5
Valentinas <vkornijenka@gmail.com>
2021-10-17 20:22:05 +1000

    changed computerPlay function. Now generates equal chance.

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 function computerPlay() {
-  for (let i = 0; i < 5; i++) {
-    const generator = Math.round(Math.random() * 9);
-    return generator <= 9 && generator > 6
-      ? "rock"
-      : generator >= 6
-      ? "scissors"
-      : "paper";
+  for (let i = 0; i < 2; i++) {
+    const generator = Math.round(Math.random() * 2);
+    return generator === 2 ? "rock" : generator === 1 ? "scissors" : "paper";
   }
 }
-function game (playerSelection, computerSelection) {
-  return playerSelection.toLowerCase() 
+
+function userInput() {
+  const input = prompt(`Rock, Paper, Scissors?`);
+  return input;
 }
 
+onload = function () {
+   userInput();
+};
 
-console.log(game("rock", computerPlay()));
+// function game(playerSelection, computerSelection) {
+//   const userScore = 0;
+//   const compScore = 0;
+//   const rounds = 0;
+//   for (let i = 0; i < 5; i++) {
+//  
+//}
+// }
+
+
