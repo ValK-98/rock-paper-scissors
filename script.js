d0f3b55803a29315336e96d89e25eaadc5d90ce4
Valentinas <vkornijenka@gmail.com>
2021-10-17 21:56:22 +1000

    Added game function

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-function computerPlay() {
-  for (let i = 0; i < 2; i++) {
-    const generator = Math.round(Math.random() * 2);
-    return generator === 2 ? "rock" : generator === 1 ? "scissors" : "paper";
-  }
-}
-
-// Placeholder for testing
-function playerPlay() {
-  for (let i = 0; i < 2; i++) {
-    const generator = Math.round(Math.random() * 2);
-    return generator === 2 ? "rock" : generator === 1 ? "scissors" : "paper";
-  }
-}
-
-// Below add in final version 
-
 // function userInput() {
 //   const input = prompt(`Rock, Paper, Scissors?`);
 //   return input;
@@ -24,49 +7,65 @@ function playerPlay() {
 //   userInput();
 // };
 
-function playRound(playerSelection, computerSelection) {
-  if (
-    playerSelection.toLowerCase() === "scissors" &&
-    computerSelection == "paper"
-  )
-    return `You win! Scissors beats paper!`;
-  else if (
-    playerSelection.toLowerCase() == "paper" &&
-    computerSelection === "rock"
-  )
-    return `You win! Paper beats rock!`;
-  else if (
-    playerSelection.toLowerCase() == "rock" &&
-    computerSelection === "scissors"
-  )
-    return `You win! Rock beats scissors!`;
-  else if (
-    playerSelection.toLowerCase() === "paper" &&
-    computerSelection === "scissors"
-  )
-    return `You lose! Scissors beats paper!`;
-  else if (
-    playerSelection.toLowerCase() === "rock" &&
-    computerSelection === "paper"
-  )
-    return `You Lose! Paper beats rock!`;
-  else if (
-    playerSelection.toLowerCase() === "scissors" &&
-    computerSelection === "rock"
-  )
-    return `You lose! Rock beats scissors!`;
-  else return "It's a tie!";
+function computerPlay() {
+  for (let i = 0; i < 2; i++) {
+    const generator = Math.round(Math.random() * 2);
+    return generator === 2 ? "rock" : generator === 1 ? "scissors" : "paper";
+  }
+}
+
+function playerPlay() {
+  // Placeholder for testing
+  for (let i = 0; i < 5; i++) {
+    const generator = Math.round(Math.random() * 2);
+    return generator === 2 ? "rock" : generator === 1 ? "scissors" : "paper";
+  }
 }
 
 const playerSelection = playerPlay();
 const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
 
-// function game(playerSelection, computerSelection) {
-//   const userScore = 0;
-//   const compScore = 0;
-//   const rounds = 0;
-//   for (let i = 0; i < 5; i++) {
-//
-//}
-// }
+function game(playerSelection, computerSelection) {
+  let userScore = 0;
+  let compScore = 0;
+  let rounds = 0;
+  for (let i = 0; i < 6; i++) {
+    console.log(`Player Score: ${userScore}  Computer Score: ${compScore}`);
+    console.log(`Round ${rounds}`);
+    // Issue with PC choice. Doesn't dynamically generate new value. Overhaul function?
+    // Seperate rock papers scissors function into separate one.
+    rounds++;
+    if (
+      (playerSelection.toLowerCase() === "scissors" &&
+        computerSelection === "paper") ||
+      (playerSelection.toLowerCase() === "rock" &&
+        computerSelection === "scissors") ||
+      (playerSelection.toLowerCase() === "paper" &&
+        computerSelection === "rock") ||
+      (playerSelection.toLowerCase() === "rock" &&
+        computerSelection === "scissors")
+    )
+      ++userScore;
+    else if (
+      (playerSelection.toLowerCase() === "rock" &&
+        computerSelection === "scissors") ||
+      (playerSelection.toLowerCase() === "paper" &&
+        computerSelection === "scissors") ||
+      (playerSelection.toLowerCase() === "rock" &&
+        computerSelection === "paper") ||
+      (playerSelection.toLowerCase() === "scissors" &&
+        computerSelection === "rock")
+    )
+      ++compScore;
+    // Need to add input for tie
+  }
+}
+
+
+console.log(game(playerSelection, computerSelection));
+
+// Left on Step 6.
+// Need to do:
+// function Game() partially functional. Point system bugged.
+// Need to add end state.
+// Remove playPlay, add back onload function for end.
