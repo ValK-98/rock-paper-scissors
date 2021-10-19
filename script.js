7231a2f171239a3943dc27969774cd8f771fd609
Valentinas <vkornijenka@gmail.com>
2021-10-19 17:57:56 +1000

    Finished functions. Need to do GUI.

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
-// function userInput() {
-//   const input = prompt(`Rock, Paper, Scissors?`);
-//   return input;
-// }
-
-// onload = function () {
-//   userInput();
-// };
+let userScore = 0;
+let compScore = 0;
+let rounds = 0;
 
 function computerPlay() {
   for (let i = 0; i < 2; i++) {
@@ -14,58 +9,50 @@ function computerPlay() {
   }
 }
 
-function playerPlay() {
-  // Placeholder for testing
-  for (let i = 0; i < 5; i++) {
-    const generator = Math.round(Math.random() * 2);
-    return generator === 2 ? "rock" : generator === 1 ? "scissors" : "paper";
-  }
+function playRound() {
+  let playerSelection = prompt(`Rock, Paper, Scissors?`);
+  let computerSelection = computerPlay();
+  if (
+    (playerSelection.toLowerCase() === "scissors" &&
+      computerSelection === "paper") ||
+    (playerSelection.toLowerCase() === "rock" &&
+      computerSelection === "scissors") ||
+    (playerSelection.toLowerCase() === "paper" &&
+      computerSelection === "rock") ||
+    (playerSelection.toLowerCase() === "rock" &&
+      computerSelection === "scissors")
+  )
+    return userScore++;
+  else if (
+    (playerSelection.toLowerCase() === "rock" &&
+      computerSelection === "scissors") ||
+    (playerSelection.toLowerCase() === "paper" &&
+      computerSelection === "scissors") ||
+    (playerSelection.toLowerCase() === "rock" &&
+      computerSelection === "paper") ||
+    (playerSelection.toLowerCase() === "scissors" &&
+      computerSelection === "rock")
+  )
+    return compScore++;
+  else console.log("It's a tie!");
 }
 
-const playerSelection = playerPlay();
-const computerSelection = computerPlay();
-
-function game(playerSelection, computerSelection) {
-  let userScore = 0;
-  let compScore = 0;
-  let rounds = 0;
+function game() {
   for (let i = 0; i < 6; i++) {
     console.log(`Player Score: ${userScore}  Computer Score: ${compScore}`);
     console.log(`Round ${rounds}`);
-    // Issue with PC choice. Doesn't dynamically generate new value. Overhaul function?
-    // Seperate rock papers scissors function into separate one.
+    playRound();
     rounds++;
-    if (
-      (playerSelection.toLowerCase() === "scissors" &&
-        computerSelection === "paper") ||
-      (playerSelection.toLowerCase() === "rock" &&
-        computerSelection === "scissors") ||
-      (playerSelection.toLowerCase() === "paper" &&
-        computerSelection === "rock") ||
-      (playerSelection.toLowerCase() === "rock" &&
-        computerSelection === "scissors")
-    )
-      ++userScore;
-    else if (
-      (playerSelection.toLowerCase() === "rock" &&
-        computerSelection === "scissors") ||
-      (playerSelection.toLowerCase() === "paper" &&
-        computerSelection === "scissors") ||
-      (playerSelection.toLowerCase() === "rock" &&
-        computerSelection === "paper") ||
-      (playerSelection.toLowerCase() === "scissors" &&
-        computerSelection === "rock")
-    )
-      ++compScore;
-    // Need to add input for tie
+    if (i === 5) endScore();
   }
 }
 
+function endScore() {
+  if (userScore > compScore) return console.log("You won!");
+  else if (userScore < compScore) return console.log("You lost!");
+  else return console.log("Wow, it's a tie!");
+}
 
-console.log(game(playerSelection, computerSelection));
-
-// Left on Step 6.
-// Need to do:
-// function Game() partially functional. Point system bugged.
-// Need to add end state.
-// Remove playPlay, add back onload function for end.
+onload = function () {
+  game();
+};
