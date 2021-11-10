a8cd2a5c536603b02aae62015d95c3cbd0856a91
Valentinas <vkornijenka@gmail.com>
2021-11-10 15:32:28 +1000

    Created functional game

--- script.js
+++ script.js
@@ -2,84 +2,125 @@ let userScore = 0;
 let compScore = 0;
 let rounds = 1;
 document.getElementById("round").innerHTML = `Pick Your Monster:`;
-document.getElementById("player-score").innerHTML = `Player Score: ${userScore}`;
-document.getElementById("computer-score").innerHTML = `Computer Score: ${compScore}`;
+document.getElementById(
+  "player-score"
+).innerHTML = `Player Score: ${userScore}`;
+document.getElementById(
+  "computer-score"
+).innerHTML = `Computer Score: ${compScore}`;
 const goblin = document.getElementById("goblin");
 const ogre = document.getElementById("ogre");
 const troll = document.getElementById("troll");
+const pcButton = document.getElementById("player-buttons");
+const endText = document.createElement("div");
 
 function computerPlay() {
   for (let i = 0; i < 2; i++) {
     const generator = Math.round(Math.random() * 2);
-    return generator === 2 ? "Rock" : generator === 1 ? "scissors" : "paper";
+    return generator === 2 ? "Ogre" : generator === 1 ? "Goblin" : "Troll";
   }
 }
 
 function playRound(playerSelection) {
   let computerSelection = computerPlay();
   if (
-    (playerSelection.toLowerCase() === "scissors" &&
-      computerSelection.toLowerCase() === "paper") ||
-    (playerSelection.toLowerCase() === "rock" &&
-      computerSelection.toLowerCase() === "scissors") ||
-    (playerSelection.toLowerCase() === "paper" &&
-      computerSelection.toLowerCase() === "rock") ||
-    (playerSelection.toLowerCase() === "rock" &&
-      computerSelection.toLowerCase() === "scissors")
+    (playerSelection.toLowerCase() === "goblin" &&
+      computerSelection.toLowerCase() === "troll") ||
+    (playerSelection.toLowerCase() === "ogre" &&
+      computerSelection.toLowerCase() === "goblin") ||
+    (playerSelection.toLowerCase() === "troll" &&
+      computerSelection.toLowerCase() === "ogre")
   )
-    return userScore++;
-    else if (
-      (playerSelection.toLowerCase() === "rock" &&
-      computerSelection.toLowerCase() === "scissors") ||
-      (playerSelection.toLowerCase() === "paper" &&
-      computerSelection.toLowerCase() === "scissors") ||
-      (playerSelection.toLowerCase() === "rock" &&
-      computerSelection.toLowerCase() === "paper") ||
-      (playerSelection.toLowerCase() === "scissors" &&
-      computerSelection.toLowerCase() === "rock")
-      )
-      return compScore++;
-      else console.log("It's a tie!");
-    }
-    
-    function game(playerSelection) {
-      for (let i = 0; i < 1; i++) {
-        playRound(playerSelection);
-        scoreDisplays();
-        npcChoice();
-        rounds++;
-        if (rounds === 6) endScore();
-      }
-    }
-    function npcChoice(){
-      document.getElementById("npc-choice").innerHTML = `Computer chose ${computerPlay()}`;
-    }
-
-    function goblinSelect() {
-      game("scissors");
-    }
-    function ogreSelect() {
-      game("scissors");
-    }
-    function trollSelect() {
-      game("paper");
-    }
-    
-    function endScore() {
-      if (userScore > compScore) return console.log("You won!");
-      else if (userScore < compScore) return console.log("You lost!");
-      else return console.log("Wow, it's a tie!");
+    return (
+      userScore++,
+      (document.getElementById(
+        "npc-choice"
+      ).innerHTML = `You win! Computer chose ${computerPlay()}.`)
+    );
+  else if (
+    (playerSelection.toLowerCase() === "ogre" &&
+      computerSelection.toLowerCase() === "troll") ||
+    (playerSelection.toLowerCase() === "troll" &&
+      computerSelection.toLowerCase() === "goblin") ||
+    (playerSelection.toLowerCase() === "goblin" &&
+      computerSelection.toLowerCase() === "ogre")
+  )
+    return (
+      compScore++,
+      (document.getElementById(
+        "npc-choice"
+      ).innerHTML = `You lose! Computer chose ${computerPlay()}.`)
+    );
+  else return (document.getElementById("npc-choice").innerHTML = `It's a tie!`);
 }
 
+function game(playerSelection) {
+  for (let i = 0; i < 1; i++) {
+    playRound(playerSelection);
+    scoreDisplays();
+    rounds++;
+    if (rounds === 6) endScore();
+  }
+}
 
+function goblinSelect() {
+  game("goblin");
+}
+function ogreSelect() {
+  game("ogre");
+}
+function trollSelect() {
+  game("troll");
+}
+
+function endScore() {
+  if (userScore > compScore) win();
+  else if (userScore < compScore) lost();
+  else return tie();
+}
 
 function scoreDisplays() {
   document.getElementById("round").innerHTML = `Round ${rounds}`;
-  document.getElementById("player-score").innerHTML = `Player Score: ${userScore}`;
-  document.getElementById("computer-score").innerHTML = `Computer Score: ${compScore}`;
+  document.getElementById(
+    "player-score"
+  ).innerHTML = `Player Score: ${userScore}`;
+  document.getElementById(
+    "computer-score"
+  ).innerHTML = `Computer Score: ${compScore}`;
 }
-
 
 goblin.addEventListener("click", goblinSelect);
 ogre.addEventListener("click", ogreSelect);
 troll.addEventListener("click", trollSelect);
+
+function lost() {
+  imgRemove();
+  endText.innerHTML = `You lost! Better luck next time!
+
+  Try again?`;
+  pcButton.append(endText);
+}
+
+function win() {
+  imgRemove();
+  endText.innerHTML = `You Won! How does it feel to win against a machine?
+
+  Try again?`;
+  pcButton.append(endText);
+}
+
+function tie() {
+  imgRemove();
+  endText.innerHTML = `It's a...Tie? Well, this is awkward.
+
+  Try again?`;
+  pcButton.append(endText);
+}
+
+function imgRemove() {
+  let computerSelection = document.getElementById("npc-choice");
+  computerSelection.remove();
+  while (pcButton.firstChild) {
+    pcButton.removeChild(pcButton.firstChild);
+  }
+}
